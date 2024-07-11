794e7cebbe5a9f5edb44e9d6f566cac37f116a9c
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-07-11 21:28:20 +0530

    authentication middleware added

--- src/routes/favoriteRoutes.js
+++ src/routes/favoriteRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { addFavorite, getFavorite } from '../controllers/favoriteController.js';
+import authenticate from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 
-router.post('/',addFavorite);
-router.get('/',getFavorite);
+router.post('/',authenticate,addFavorite);
+router.get('/',authenticate,getFavorite);
 
 export default router;
--- src/routes/weatherRoutes.js
+++ src/routes/weatherRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import { getCurrentWeather, getForecast, getHistorical } from '../controllers/weatherController.js';
+import authenticate from '../middleware/authMiddleware.js';
 
 
 
 const router = express.Router();
 
 
-router.get('/current',getCurrentWeather);
-router.get('/forecast',getForecast);
-router.get('/historical',getHistorical); 
+router.get('/current',authenticate,getCurrentWeather);
+router.get('/forecast',authenticate,getForecast);
+router.get('/historical',authenticate,getHistorical); 
 
 export default router;
